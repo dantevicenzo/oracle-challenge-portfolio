849196f73b2213e62166f7cf1c81f40133698144
Dante Vicenzo <dantevcz@gmail.com>
2023-06-14 13:53:25 -0300

    Hero links update

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,17 +12,29 @@ export function Hero() {
         </Paragraph>
         <LinksList>
           <li>
-            <a href="#">
+            <a
+              href="https://github.com/dantevicenzo/"
+              target="_blank"
+              rel="noreferrer"
+            >
               Github <img src={ArrowUpRight} alt="" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              href="https://www.linkedin.com/in/dantevicenzo/"
+              target="_blank"
+              rel="noreferrer"
+            >
               Linkedin <img src={ArrowUpRight} alt="" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              href="https://www.instagram.com/dantevicenzo"
+              target="_blank"
+              rel="noreferrer"
+            >
               Instagram <img src={ArrowUpRight} alt="" />
             </a>
           </li>
