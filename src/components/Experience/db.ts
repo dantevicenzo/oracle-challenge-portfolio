4da55304206b24a8873f4ac34591a8b521098c09
Dante Vicenzo <dantevcz@gmail.com>
2023-07-11 12:43:19 -0300

    Experience list code refactor using db

---
+++ src/components/Experience/db.ts
@@ -0,0 +1,52 @@
+import experience1 from '../../assets/experience1.png'
+import experience2 from '../../assets/experience2.png'
+import experience3 from '../../assets/experience3.png'
+import experience4 from '../../assets/experience4.png'
+import experience5 from '../../assets/experience5.png'
+import experience6 from '../../assets/experience6.png'
+
+export const experienceList = [
+  {
+    imageUrl: experience1,
+    title: 'Decodificador de texto',
+    languages: 'HTML, CSS e Javascript',
+    repositoryUrl:
+      'https://github.com/dantevicenzo/oracle-challenge-decodificador',
+    demoUrl: 'https://dantevicenzo.github.io/oracle-challenge-decodificador',
+  },
+  {
+    imageUrl: experience2,
+    title: 'Lista de Tarefas',
+    languages: 'React e Typescript',
+    repositoryUrl: 'https://github.com/dantevicenzo/todo-list',
+    demoUrl: 'https://todo-list-7wc.pages.dev/',
+  },
+  {
+    imageUrl: experience3,
+    title: 'Cafeteria Delivery',
+    languages: 'React e Typescript',
+    repositoryUrl: 'https://github.com/dantevicenzo/coffee-delivery',
+    demoUrl: 'https://coffee-delivery.pages.dev/',
+  },
+  {
+    imageUrl: experience4,
+    title: 'Github Blog',
+    languages: 'React e Typescript',
+    repositoryUrl: 'https://github.com/dantevicenzo/github-blog',
+    demoUrl: 'https://github-blog-prod.pages.dev/',
+  },
+  {
+    imageUrl: experience5,
+    title: 'Ignite Shop',
+    languages: 'React, Typescript e Next.js',
+    repositoryUrl: 'https://github.com/dantevicenzo/ignite-shop',
+    demoUrl: 'https://ignite-shop-fawn.vercel.app/',
+  },
+  {
+    imageUrl: experience6,
+    title: 'Alura Geek',
+    languages: 'React, Typescript e Next.js',
+    repositoryUrl: 'https://github.com/dantevicenzo/alura-geek',
+    demoUrl: 'https://alura-geek-taupe.vercel.app/',
+  },
+]
--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,9 +1,4 @@
-import experience1 from '../../assets/experience1.png'
-import experience2 from '../../assets/experience2.png'
-import experience3 from '../../assets/experience3.png'
-import experience4 from '../../assets/experience4.png'
-import experience5 from '../../assets/experience5.png'
-import experience6 from '../../assets/experience6.png'
+import { experienceList } from './db'
 import {
   ActionsWrapper,
   Container,
@@ -20,144 +15,28 @@ export function Experience() {
     <Container id="experience">
       <h2>Experiência profissional</h2>
       <List>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience1} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Decodificador de texto</strong>
-            <span>HTML, CSS e Javascript</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/oracle-challenge-decodificador"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://dantevicenzo.github.io/oracle-challenge-decodificador"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience2} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Lista de Tarefas</strong>
-            <span>React e Typescript</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/todo-list"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://todo-list-7wc.pages.dev/"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience3} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Cafeteria Delivery</strong>
-            <span>React e Typescript</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/coffee-delivery"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://coffee-delivery.pages.dev/"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience4} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Github Blog</strong>
-            <span>React e Typescript</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/github-blog"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://github-blog-prod.pages.dev/"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience5} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Ignite Shop</strong>
-            <span>React, Typescript e Next.js</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/ignite-shop"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://ignite-shop-fawn.vercel.app/"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
-        <ListItem>
-          <ImgWrapper>
-            <img src={experience6} alt="" />
-          </ImgWrapper>
-          <InfoWrapper>
-            <strong>Alura Geek</strong>
-            <span>React, Typescript e Next.js</span>
-            <ActionsWrapper>
-              <RepositoryButton
-                href="https://github.com/dantevicenzo/alura-geek"
-                target="_blank"
-              >
-                Repositório
-              </RepositoryButton>
-              <SeeDemoButton
-                href="https://alura-geek-taupe.vercel.app/"
-                target="_blank"
-              >
-                Ver demo
-              </SeeDemoButton>
-            </ActionsWrapper>
-          </InfoWrapper>
-        </ListItem>
+        {experienceList.map((experience, index) => (
+          <ListItem key={index}>
+            <ImgWrapper>
+              <img src={experience.imageUrl} alt="" />
+            </ImgWrapper>
+            <InfoWrapper>
+              <strong>{experience.title}</strong>
+              <span>{experience.languages}</span>
+              <ActionsWrapper>
+                <RepositoryButton
+                  href={experience.repositoryUrl}
+                  target="_blank"
+                >
+                  Repositório
+                </RepositoryButton>
+                <SeeDemoButton href={experience.demoUrl} target="_blank">
+                  Ver demo
+                </SeeDemoButton>
+              </ActionsWrapper>
+            </InfoWrapper>
+          </ListItem>
+        ))}
       </List>
     </Container>
   )
